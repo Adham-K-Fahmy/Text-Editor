3001a636eeefd1cd709e72bc681b90627659e4b3
Adham-K-Fahmy <95886599+Adham-K-Fahmy@users.noreply.github.com>
2022-05-12 14:20:06 +0200

    added 5 function calls
    1- add new text
    2- print file
    3- empty file
    4- encrypt file
    5- decrypt file

--- texteditor.h
+++ texteditor.h
@@ -1,7 +1,12 @@
 #ifndef TEXTEDITOR_H_INCLUDED
 #define TEXTEDITOR_H_INCLUDED
 
+using namespace std;
 
-
+void addNewText(string &data);
+void printFile(string fileName);
+void emptyFile(string fileName);
+void encryptFile(string &data);
+void decryptFile(string &data);
 
 #endif // TEXTEDITOR_H_INCLUDED
