993df7a893066e199a0365808b6bde5510bf2e7f
AbdoAqll <abdonasraqll@gmail.com>
2022-05-14 14:53:18 +0200

    committed headers for the functions

--- texteditor.h
+++ texteditor.h
@@ -8,5 +8,8 @@ void printFile(string fileName);
 void emptyFile(string fileName);
 void encryptFile(string &data);
 void decryptFile(string &data);
-
+void split (string line);
+void countWord();
+string toUpper(string line);
+string toLower(string line);
 #endif // TEXTEDITOR_H_INCLUDED
